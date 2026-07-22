0f6f6d7e430a0ddf7b27616c7a424ed04f7e72b3
Farhod Gofurov <fg325609@gmail.com>
2026-07-22 15:00:40 +0500

    yana qoshildi

--- src/app/companents/Header/Header.jsx
+++ src/app/companents/Header/Header.jsx
@@ -1,8 +1,20 @@
+import Link from 'next/link'
 import React from 'react'
 
 const Header = () => {
   return (
-    <div>header</div>
+    <>
+
+    <header>
+        <nav>
+<Link href="/">Bosh sahifa </Link>
+<Link href="/about">Biz haqimizda</Link>
+<Link href="/tours">Turlar</Link>
+<Link href="/contact">Kontaktlar</Link>
+<Link href="/form">Forma</Link>
+        </nav>
+    </header>
+    </>
   )
 }
 
