c440b469b41a1d6bde03a245c506a39e8d19071d
Mahliyo <gasandigital13@gmail.com>
2026-07-23 22:36:36 +0500

    hero responsive style swipper kyota

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import Hero from './companents/Hero/Hero'
 import Travels from './companents/Travels/Travels'
+import Offer from './companents/Offer/Offer'
 const Home = () => {
   return (
     <>
 
     <Hero/>
     <Travels/>
+    <Offer/>
     </>
   )
 }
