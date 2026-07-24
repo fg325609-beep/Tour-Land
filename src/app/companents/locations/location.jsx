a196baf5ae5ebd373c580ce7e3aff943e478233c
Akmalov-dev <amirbekdebruyne@gmail.com>
2026-07-24 18:52:10 +0500

    location

--- src/app/companents/locations/location.jsx
+++ src/app/companents/locations/location.jsx
@@ -6,7 +6,7 @@ const location = () => {
        <p className='text-black text-5xl font-bold text-center'>
        Eng Yaxshi Manzillar
        </p>
-       <p className='text-gray-400 w-180 text-center ml-92 mt-10 text-xl'>Har bir shahardagi eng yaxshi takliflarimizni kashf eting, faqat siz uchun. Ozingiz uchun yangi dunyosni oching va misli korilmagan sarguzashtlar ega boling.</p>
+       <p className='text-gray-410 w-180 text-center ml-92 mt-10 text-xl'>Har bir shahardagi eng yaxshi takliflarimizni kashf eting, faqat siz uchun. Ozingiz uchun yangi dunyosni oching va misli korilmagan sarguzashtlar ega boling.</p>
 
        <div>
         
