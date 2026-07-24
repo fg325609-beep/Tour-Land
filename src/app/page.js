cd281059f454474344cee68929720a848324e463
Akmalov-dev <amirbekdebruyne@gmail.com>
2026-07-24 18:50:48 +0500

    location bo'limi

--- src/app/page.js
+++ src/app/page.js
@@ -4,15 +4,16 @@ import Travels from './companents/Travels/Travels'
 import Offer from './companents/Offer/Offer'
 import Tickets from './companents/Tickets/Tickets'
 import Interesting from './companents/Interesting/Interesting'
+import Location from "../app/companents/locations/location"
 const Home = () => {
   return (
     <>
-
     <Hero/>
     <Travels/>
     <Offer/>
     <Tickets/>
     <Interesting/>
+    <Location/>
     </>
   )
 }
